d5908327b04eed688a0747b1e89240b21c66541e
Kirill Vercetti <office@kyzima-spb.com>
2023-08-23 16:34:41 +0300

    Switch to non-root user

--- docker/root/usr/local/bin/docker-entrypoint.sh
+++ docker/root/usr/local/bin/docker-entrypoint.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
-log() {
-  echo "* $1" >&2
-}
 
+if ! getent passwd "$(id -u)" > /dev/null 2>&1; then
+  export HOME=/tmp
+fi
 
 CORDOVA_TELEMETRY_OFF=${CORDOVA_TELEMETRY_OFF:-'0'}
 export CORDOVA_TELEMETRY_OFF
 [[ $CORDOVA_TELEMETRY_OFF != '0' ]] && cordova telemetry off > /dev/null
 
-
-log "JDK:     $(java -version 2>&1 | grep version)"
-log "Gradle:  $(gradle -v | grep ^Gradle | sed 's/Gradle //')"
-log "Cordova: $(cordova --no-telemetry -v)"
 
 exec "$@"
